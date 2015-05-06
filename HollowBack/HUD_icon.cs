ea6dc179c466d91d7484683073c192ef63ea5f74
Pedro Miguel Leal de Carvalho <tunafishmessiah@gmail.com>
2015-05-06 15:24:14 +0100

    HUD changes
    Start to change the hud, gona make it them have their own class so they
    can have their own atributes.
    Unfinished

---
+++ HollowBack/HUD_icon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+namespace HollowBack
+{
+    class HUD_icon : Sprite
+    {
+        private SpriteFont font;
+        private int function;
+        //0-Missils
+        //1-Lasers
+        //2-Railgun
+        //3-Particle Cannon
+        //4-EMP
+        //5-Torpedos
+        public HUD_icon(ContentManager pContent, Scene scene, int func)
+            : base(pContent, "SideBlocks",scene)
+        {
+            this.function = func;
+        }
+    }
+
+    //mouseState.LeftButton == ButtonState.Pressed
+}
