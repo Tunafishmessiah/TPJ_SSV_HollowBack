0ad1dab29a0af3adb7f330e7fb12c57ac373b719
Pedro Miguel Leal de Carvalho <tunafishmessiah@gmail.com>
2015-05-07 19:21:18 +0100

    MORE HUD CHANGES
    MADE THE HUD PRETTY
    SOO PROUD

--- HollowBack/HUD_icon.cs
+++ HollowBack/HUD_icon.cs
@@ -14,26 +14,73 @@ namespace HollowBack
     {
         private SpriteFont font;
         private string function;
+        private Texture2D original,Lighty;
+        private bool lighty;
+        private bool Click;
+        private MouseState previous;
+        private MouseState actualMouse;
+
 
         public HUD_icon(ContentManager pContent, Scene scene, int func)
             : base(pContent, "SideBlocks",scene)
         {
             font = pContent.Load<SpriteFont>("RadioLand");
-
+            original = pContent.Load<Texture2D>("SideBlocks");
+            Lighty = pContent.Load<Texture2D>("SideLight");
             HUD_Func(func);
+            lighty = false;
+            Click = false;
+            actualMouse = Mouse.GetState();
+            previous = actualMouse;
         }
+
+        public override void Update(GameTime pGameTime)
+        {
+            Vector2 mouse = scene.Little.Position;
+            actualMouse = Mouse.GetState();
+
+            if (actualMouse.LeftButton == ButtonState.Pressed && previous.LeftButton == ButtonState.Released && OnTop(mouse))
+            {
+                Click = !Click;
+                if (Click)
+                {
+                    foreach (HUD_icon HUD in scene.HUD)
+                    {
+                        if (HUD != this)
+                        HUD.Click = false;
+                    }
+                }
+            }
+
+            if (OnTop(mouse) || Click)
+            {
+                lighty = true;
+            }
+            else lighty = false;
+
+
+
+            if (lighty) this.Texture = Lighty;
+            else this.Texture = original;
+
+            base.Update(pGameTime);
+
+            previous = actualMouse;
+        }
+
         public override void Draw(SpriteBatch pSpriteBatch)
         {
             base.Draw(pSpriteBatch);
             scene.SpriteBatch.DrawString(font, function,
-                new Vector2(this.Position.X + 10, this.Position.Y + (this.Texture.Height / 2) 
-                    - (this.font.MeasureString(function).Y/2)),Color.Red);
+                new Vector2(this.Position.X + 10, this.Position.Y + (this.Texture.Height / 2)
+                    - (this.font.MeasureString(function).Y / 2)), Color.Red);
         }
+
         private void HUD_Func(int function)
         {
-            switch(function)
+            switch (function)
             {
-                case(0):
+                case (0):
                     this.function = "Missil";
                     break;
                 case (1):
@@ -53,7 +100,14 @@ namespace HollowBack
                     break;
             }
         }
+        
+        private bool OnTop(Vector2 Position)
+        {
+            if (this.Position.X < Position.X && this.Position.X + this.Texture.Width> Position.X && this.Position.Y < Position.Y && this.Position.Y + this.Texture.Height > Position.Y)
+            {
+                return true;
+            }
+            return false;
+        }
     }
-
-    //mouseState.LeftButton == ButtonState.Pressed
 }
