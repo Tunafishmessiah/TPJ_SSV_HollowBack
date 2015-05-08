165a41533e9198185ef67954bf05f42f3eeb6361
Filipe Luís Grave Camacho <camacho_pt@hotmail.com>
2015-05-08 12:41:59 +0100

    More ladar changes

--- HollowBack/Ladar.cs
+++ HollowBack/Ladar.cs
@@ -19,23 +19,34 @@ namespace HollowBack
             this.Position = new Vector2(this.scene.ScreenSize.X / 2, this.scene.ScreenSize.Y / 2);         
         }
 
-        public  void Update(GameTime pGameTime, bool lockin, Point stop)
+        public  void Update(GameTime pGameTime, bool lockin, float stopAngle)
         {
             Point mouse = scene.Mstate.Position;
 
+           // if (Math.Atan((-mouse.X / mouse.Y)) < (Math.Atan((-stop.X / stop.Y)) + Math.PI / 16) && Math.Atan((-mouse.X / mouse.Y)) > (Math.Atan((-stop.X / stop.Y)) - Math.PI / 16))
             if (lockin == true)
             {
-                if (Math.Atan((-mouse.X / mouse.Y)) < (Math.Atan((-stop.X / stop.Y)) + Math.PI / 16) && Math.Atan((-mouse.X / mouse.Y)) > (Math.Atan((-stop.X / stop.Y)) - Math.PI / 16))
+                if ((this.angle + 0.4) >= stopAngle + Math.PI / 32 && (this.angle - 0.4) <= stopAngle - Math.PI / 32)
                 {
                     float a = (float)(mouse.X - this.scene.ScreenSize.X / 2);
                     float l = (float)(mouse.Y - this.scene.ScreenSize.Y / 2);
                     this.angle = (float)(Math.Atan2(l, a));
-                    Console.WriteLine(this.angle);
+                    //Console.WriteLine(this.angle);
                     base.Update(pGameTime);
                 }
+                else
+                {
+
+                }
             }
-
-
+            else
+            {
+                float a = (float)(mouse.X - this.scene.ScreenSize.X / 2);
+                float l = (float)(mouse.Y - this.scene.ScreenSize.Y / 2);
+                this.angle = (float)(Math.Atan2(l, a));
+                //Console.WriteLine(this.angle);
+                base.Update(pGameTime);
+            }
         }
 
         public override void Draw(SpriteBatch pSpriteBatch)
