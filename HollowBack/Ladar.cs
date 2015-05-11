c8d9d6b1dd84fe5c6dfb23c174b77ca837a02847
Filipe Luís Grave Camacho <camacho_pt@hotmail.com>
2015-05-11 15:57:51 +0100

    Ladar tweak, still buggy

--- HollowBack/Ladar.cs
+++ HollowBack/Ladar.cs
@@ -11,23 +11,31 @@ namespace HollowBack
 {
     class Ladar:Sprite
     {
-        
+
+        #region Fields
+
+        private float prevAngle = 0;
+
+        #endregion
+
         public Ladar(ContentManager pContent, Scene Cene)
             : base(pContent, "Ladar", Cene)
         {
-            this.Origin = new Vector2(this.Texture.Width/2-5, this.Texture.Height/2+5);
-            this.Position = new Vector2(this.scene.ScreenSize.X / 2, this.scene.ScreenSize.Y / 2);         
+            this.Origin = new Vector2(this.Texture.Width/2-4, this.Texture.Height/2+5);
+            this.Position = new Vector2(this.scene.ScreenSize.X / 2+3, this.scene.ScreenSize.Y / 2);
         }
 
         public  void Update(GameTime pGameTime, bool lockin, float stopAngle)
         {
             Point mouse = scene.Mstate.Position;
+            
 
            // if (Math.Atan((-mouse.X / mouse.Y)) < (Math.Atan((-stop.X / stop.Y)) + Math.PI / 16) && Math.Atan((-mouse.X / mouse.Y)) > (Math.Atan((-stop.X / stop.Y)) - Math.PI / 16))
             if (lockin == true)
             {
-                if ((this.angle + 0.4) >= stopAngle + Math.PI / 32 && (this.angle - 0.4) <= stopAngle - Math.PI / 32)
+                if ((this.angle + 0.4) > stopAngle + Math.PI / 32 && (this.angle - 0.4) < stopAngle - Math.PI / 32)
                 {
+                    prevAngle = this.angle;
                     float a = (float)(mouse.X - this.scene.ScreenSize.X / 2);
                     float l = (float)(mouse.Y - this.scene.ScreenSize.Y / 2);
                     this.angle = (float)(Math.Atan2(l, a));
@@ -36,7 +44,7 @@ namespace HollowBack
                 }
                 else
                 {
-
+                    this.angle = prevAngle;
                 }
             }
             else
