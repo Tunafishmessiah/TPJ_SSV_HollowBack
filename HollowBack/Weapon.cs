cc0df98536f76cedfe6f74069cf8666ec36dd5ed
CommanderB95 <bruno.luis.73@hotmail.com>
2015-05-14 17:03:25 +0100

    Created Weapon Class
    Created weapon class to manage each ships weapons.

---
+++ HollowBack/Weapon.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace HollowBack
+{
+    class Weapon
+    {
+        #region Fields
+
+        private float cooldown;
+        private float range;
+        private float repairTime;
+
+        #endregion
+
+        #region Properties
+
+        public float Cooldown
+        {
+            get { return cooldown; }
+            protected set { cooldown = value; }
+        }
+
+        public float Range
+        {
+            get { return range; }
+            protected set { range = value; }
+        }
+
+        public float RepairTime
+        {
+            get { return repairTime; }
+            protected set { repairTime = value; }
+        }
+
+        #endregion
+
+        public Weapon()
+        {
+
+        }
+
+        //public void FireWeapon();
+
+        //public void Repair();
+
+        public void Update(Vector2 pPosition, Vector2 pTarget)
+        {
+            if (RepairTime <= 0f)
+            {
+                if (Cooldown <= 0f)
+                {
+                    if (Vector2.Distance(pPosition, pTarget) < Range) ;
+                        //FireWeapon();
+                }
+                else Cooldown -= 0.1f;
+            }
+            else RepairTime -= 0.1f;
+        }
+    }
+}
