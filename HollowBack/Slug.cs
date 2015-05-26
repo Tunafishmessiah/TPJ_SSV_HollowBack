ee7c257d50350e5961888e2cabd8b02d006028ee
CommanderB95 <bruno.luis.73@hotmail.com>
2015-05-26 15:12:35 +0100

    Added Missile and Slug
    Added missile and slug to the game.

---
+++ HollowBack/Slug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace HollowBack
+{
+    class Slug : Enemy
+    {
+        public Slug(ContentManager pContent, Scene pScene)
+            : base(pContent, "Hunter", pScene)
+        {
+            IsUnknown = false;
+            IsVisible = true;
+            IsActive = false;
+
+            MaxSpeed = 2f;
+            Velocity = MaxSpeed;
+            Accelaration = 0.2f;
+        }
+    }
+}
